a51955f0e9269b681bf0a5eb3295feea1316ab55
Zmoore <zmoore371@gmail.com>
2022-01-11 20:18:03 -0500

    created models for user and authentication, created files for gql schema and index

---
+++ server/models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    //string with no spaces for username, must be unique
+    username: {
+        type: String,
+        required: true, 
+        unique: true,
+        trim: true,
+    },
+    //email
+    email: {
+        type: String,
+        required: true, 
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    //password must be 8 characters or longer
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    
+    //this is to reference items for particular user
+    items: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Item'
+        },
+    ],
+});
+
+userSchema.pre('save', async function (next) {
+    if(this.isNew || this.isModified('password')) {
+        // encryption cost factor
+        const saltRounds = 10
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model(User, userSchema)
+module.exports = User;
