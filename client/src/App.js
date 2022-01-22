20cd7ef510e1d5910ee9ad4d9d769bf4fac11d48
Terrence Meikle <terrence_meikle@yahoo.com>
2022-01-22 10:46:37 -0500

    added logout and misc functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from '@apollo/client';
 
-import { setContext } from '@apollo/client/link/context';
+// import { setContext } from '@apollo/client/link/context';
+
+// import { useCurrentUserContext } from '../../context/auth-context';
+import { useCurrentUserContext } from './context/auth-context';
+import { useQuery } from '@apollo/client';
+// import { QUERY_LOAD_ALL } from '../../utils/queries';
+import { QUERY_LOAD_ALL } from './utils/queries';
 
 import { Fragment, useState } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import CurrentUserProvider from './context/auth-context';
+import { Route, Switch } from 'react-router-dom';
+// import CurrentUserProvider from './context/auth-context';
 
 import Header from './components/Header/Header';
 import MainSection from './components/Main/MainSection';
@@ -19,32 +25,21 @@ import AddSaleItem from './components/SaleItems/AddSaleItem/AddSaleItem';
 import SellersModalPage from './pages/SellersModalPage/SellersModalPage';
 import NotFound from './pages/NotFound';
 
-// Construct our main GraphQL API endpoint
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-// Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
 function App() {
   const [modalIsShow, setModalIsShown] = useState(false);
+  const { loading, data } = useQuery(QUERY_LOAD_ALL);
+
+  console.log('data ++', data);
+
+  const loadAllQuery = {
+    loading,
+    data,
+  };
+
+  const {
+    currentUser: { isLoggedIn },
+  } = useCurrentUserContext();
+  console.log(isLoggedIn);
 
   const logInHandler = (e) => {
     console.log(e);
@@ -69,38 +64,38 @@ function App() {
 
   return (
     <Fragment>
-      <ApolloProvider client={client}>
-        <CurrentUserProvider>
-          <Switch>
-            <Route path="/" exact>
-              {modalIsShow && (
-                <SignUp
-                  onClose={hideModalhandler}
-                  onSignUpSubmit={signUpSubmitHandler}
-                />
-              )}
-              <Header
-                onSignup={showModalHandler}
-                onLogIn={logInHandler}
-                onLogInSubmit={logInSubmitHandler}
-              />
-              <MainSection />
-            </Route>
-            <Route path="/profile-page">
-              <ProfilePage />
-            </Route>
-            <Route path="/add-sale-item">
-              <AddSaleItem />
-            </Route>
-            <Route path="/seller-modal">
-              <SellersModalPage onClose={hideModalhandler} />
-            </Route>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
-        </CurrentUserProvider>
-      </ApolloProvider>
+      {/* <ApolloProvider client={client}> */}
+      {/* <CurrentUserProvider> */}
+      <Switch>
+        <Route path="/" exact>
+          {modalIsShow && (
+            <SignUp
+              onClose={hideModalhandler}
+              onSignUpSubmit={signUpSubmitHandler}
+            />
+          )}
+          <Header
+            onSignup={showModalHandler}
+            onLogIn={logInHandler}
+            onLogInSubmit={logInSubmitHandler}
+          />
+          <MainSection loadAllQuery={loadAllQuery} />
+        </Route>
+        <Route path="/profile-page">
+          <ProfilePage />
+        </Route>
+        <Route path="/add-sale-item">
+          <AddSaleItem />
+        </Route>
+        <Route path="/seller-modal">
+          <SellersModalPage onClose={hideModalhandler} />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+      {/* </CurrentUserProvider> */}
+      {/* </ApolloProvider> */}
     </Fragment>
   );
 }
