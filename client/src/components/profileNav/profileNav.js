adbe230a8093fb4503e484e5fe300d27fc7ee791
Philip <corewired741@outlook.com>
2022-01-22 02:34:05 +0000

    Added profile widget to components

---
+++ client/src/components/profileNav/profileNav.js
@@ -0,0 +1,55 @@
+import {
+    IconButton, Avatar, Box, Flex, HStack, VStack, Text, Menu, MenuButton, MenuDivider,
+    MenuItem, MenuList,
+  } from "@chakra-ui/react";
+  import { FiChevronDown, FiBell } from "react-icons/fi";
+  
+  export default function ProfileNav() {
+    return (
+      <HStack spacing={{ base: "0", md: "6" }}>
+        <IconButton
+          size="lg"
+          variant="ghost"
+          aria-label="open menu"
+          icon={<FiBell />}
+        />
+        <Flex alignItems="center">
+          <Menu>
+            <MenuButton
+              py={2}
+              transition="all 0.3s"
+              _focus={{ boxShadow: "none" }}
+            >
+              <HStack spacing="4">
+                <Avatar
+                  size="md"
+                  src={
+                    "https://storage.googleapis.com/project3-marketplace/sky-background.png"
+                  }
+                />
+                <VStack
+                  display={{ base: "none", md: "flex" }}
+                  alignItems="flex-start"
+                  spacing="1px"
+                  ml="2"
+                >
+                  <Text fontSize="lg">User Name</Text>
+                  <Text fontSize="md" color="gray.600">
+                    Normal User
+                  </Text>
+                </VStack>
+                <Box display={{ base: "none", md: "flex" }}>
+                  <FiChevronDown />
+                </Box>
+              </HStack>
+            </MenuButton>
+            <MenuList fontSize="lg" bg="white" borderColor="gray.200">
+              <MenuItem>Profile</MenuItem>
+              <MenuDivider />
+              <MenuItem>Sign out</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </HStack>
+    );
+  }
